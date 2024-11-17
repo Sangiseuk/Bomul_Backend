72215bd9055e4cd1e3a227620130f4d4d1072409
이용태 <ioosulsa@gmail.com>
2024-11-17 19:40:11 +0900

    [Create] init Spring Boot Project

---
+++ src/main/java/com/example/bomul_backend/BomulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bomul_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomulBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bomul_backend/BomulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bomul_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
