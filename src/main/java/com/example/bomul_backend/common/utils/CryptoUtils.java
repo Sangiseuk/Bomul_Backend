76679485d0448a77a78e16cf35012b7bff4b4cbe
오상하 <tkdgk1204@naver.com>
2024-11-22 13:22:17 +0900

    [Modify] signup

--- src/main/java/com/example/bomul_backend/common/utils/CryptoUtils.java
+++ src/main/java/com/example/bomul_backend/common/utils/CryptoUtils.java
@@ -1,5 +1,20 @@
 package com.example.bomul_backend.common.utils;
 
+import java.security.MessageDigest;
+
 public class CryptoUtils {
-    private CryptoUtils() { throw new UnsupportedOperationException("Cannot be instantiated"); }
+    private CryptoUtils() {
+        throw new UnsupportedOperationException("Cannot be instantiated");
+    }
+
+    public static byte[] getSHA256(String source, String salt){
+        byte[] byteData = null;
+        try{
+            MessageDigest md = MessageDigest.GetInstance("SHA-256");
+            md.update(source.getBytes());
+            md.update(salt.getBytes());
+            byteData=md.
+        }
+    }
+
 }
